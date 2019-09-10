690cd2ec5de2ea25b735bd296159c44032c03579
MURILO DO COUTO SANTOS <21634@unifacef.edu.br>
2019-09-09 22:10:20 -0300

    Autualizado com funcao

--- Vertor com objeto/Exercicio01/exercicio01.js
+++ Vertor com objeto/Exercicio01/exercicio01.js
@@ -1,10 +1,9 @@
 function funcao01() {
-
     var Vendas = Array();
     var vendedor = Array();
     var opcao, venpcod = 0, Auxvendas = 0, auxvendedor, auxvendaspmes;
     var i = 0;
-    var cola = "";
+
 
 
     do {
@@ -22,147 +21,44 @@ function funcao01() {
 
             case 1:
 
-                for (i = 0; i <= 4; i++) {
+                cadastraVendedor(vendedor);
 
-                    var informacao = {};
-
-                    informacao.codigo = Number(prompt("Coloque o codigo"));
-                    informacao.nome = prompt("Coloque o nome");
-
-                    vendedor[i] = informacao;
-
-                    if ((i > 0) && (vendedor[i].codigo != vendedor[i - 1].codigo)) {
-
-                        alert("Cadastrado com sucesso");
-
-                    } else if (i != 0) {
-
-                        alert("Codigo ja cadastrado");
-
-                        i--;
-
-                    } else if (i == 0) {
-
-                        alert("Cadastrado com sucesso");
-
-                    }
-
-                }
                 break;
+
 
             case 02:
 
-
-
-                for (i = 0; i < vendedor.length; i++) {
-
-                    cola += vendedor[i].codigo + " - " + vendedor[i].nome + "\n";
-
-                }
-                do {
-
-                    var informacao = {};
-
-                    informacao.codven = Number(prompt(cola + "\nColoque o codigo do vendedor"));
-                    informacao.mes = Number(prompt("Coloque o mes"));
-                    informacao.Vendas = Number(prompt("Coloque a vendas"));
-
-                    Vendas[i] = informacao;
-
-                    if ((i > 0) && (Vendas[i].codven == Vendas[i - 1].codven) && (Vendas[i].mes == Vendas[i - 1].mes)) {
-
-                        alert("Não pode ter um vendendor com duas vendas no mesmo mes");
-                        i--;
-
-                    } else {
-                        alert("Cadastrado com sucesso");
-                        i++;
-
-                    }
-
-                    var Fin = Number(prompt("Terminou de cadastrar as vendas 0 - Sim 1 - Não"));
-
-
-                } while (Fin != 0);
+                cadastraVendas(vendedor, Vendas);
 
                 break;
 
+
             case 03:
 
+                consultaVenda(Vendas);
 
-                var codver = Number(prompt("Coloque o codigo"));
-                var codmes = Number(prompt("Coloque o mes"));
-                for (i = 0; i < Vendas.length; i++) {
-
-                    if ((codver == Vendas[i].codven) && (codmes == Vendas[i].mes)) {
-
-                        venpcod = venpcod + Vendas[i].Vendas;
-
-                    }
-
-
-                }
-                alert("Vendas feita pelo " + codver + " no mes " + codmes + " : " + venpcod);
 
                 break;
 
             case 04:
 
-                var codver = Number(prompt("Coloque o codigo"));
-
-                for (i = 0; i <= Vendas.length; i++) {
-
-                    if (codver == Vendas[i].codven) {
-
-                        venpcod += Vendas[i].Vendas;
-
-                    }
-
-
-                }
-                alert("Vendas feita pelo " + codver + " : " + venpcod);
+                consultaTotalVendas(Vendas);
 
                 break;
             case 05:
 
-                var codmes = Number(prompt("Coloque o mes"));
-
-                Auxvendas = Vendas[0].Vendas;
-
-                for (i = 0; i < Vendas.length; i++) {
-
-                    if ((codmes == Vendas[i].mes) && (Auxvendas <= Vendas[i].Vendas)) {
-
-                        Auxvendas = Vendas[i].codven;
-
-                        auxvendedor = Vendas[i].codven;
-
-                    }
-
-
-                }
-                alert("O vendedor que vendeu mais: " + auxvendedor);
+                calcularMaisVendeu(Vendas);
 
                 break;
 
             case 06:
 
-                var messes = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-                for (i = 0; i < Vendas.length; i++) {
+                calculaMesMaisVendeu(Vendas);
 
-                    messes[vendas[i].mes - 1] = messes[Vendas[i].mes - 1] + Vendas[i].valor;
+                break;
 
-                }
-                var maior = 0;
-                var mesMaior = 0;
-
-
-                for (i = 0; i < messes.length; i++) {
-
-                    maior = messes[i];
-                    mesMaior = i+1;
-
-                }
+            case 07:
+                alert("Obrigado pela preferencia");
 
                 break;
 
@@ -178,4 +74,162 @@ function funcao01() {
 
 
     } while (opcao != 7);
+
+}
+
+function cadastraVendedor(vendedor) {
+    
+
+    for (i = 0; i <= 4; i++) {
+
+        var informacao = {};
+
+        informacao.codigo = Number(prompt("Coloque o codigo"));
+        informacao.nome = prompt("Coloque o nome");
+
+        vendedor[i] = informacao;
+
+        if ((i > 0) && (vendedor[i].codigo != vendedor[i - 1].codigo)) {
+
+            alert("Cadastrado com sucesso");
+
+        } else if (i != 0) {
+
+            alert("Codigo ja cadastrado");
+
+            i--;
+
+        } else if (i == 0) {
+
+            alert("Cadastrado com sucesso");
+
+        }
+
+    }
+
+
+}
+function cadastraVendas(vendedor, Vendas) {
+
+
+    var cola = funCola(vendedor);
+
+    do {
+
+        var informacao = {};
+
+        informacao.codven = Number(prompt(cola + "\nColoque o codigo do vendedor"));
+        informacao.mes = Number(prompt("Coloque o mes"));
+        informacao.Vendas = Number(prompt("Coloque a vendas"));
+
+        Vendas[i] = informacao;
+
+        if ((i > 0) && (Vendas[i].codven == Vendas[i - 1].codven) && (Vendas[i].mes == Vendas[i - 1].mes)) {
+
+            alert("Não pode ter um vendendor com duas vendas no mesmo mes");
+            i--;
+
+        } else {
+            alert("Cadastrado com sucesso");
+            i++;
+
+        }
+
+        var Fin = Number(prompt("Terminou de cadastrar as vendas 0 - Sim 1 - Não"));
+
+
+    } while (Fin != 0);
+
+}
+function consultaVenda(Vendas) {
+
+    var cola = funCola(vendedor);
+
+    var codver = Number(prompt("Coloque o codigo\n"+cola));
+    var codmes = Number(prompt("Coloque o mes"));
+
+    for (i = 0; i < Vendas.length; i++) {
+
+        if ((codver == Vendas[i].codven) && (codmes == Vendas[i].mes)) {
+
+            venpcod = venpcod + Vendas[i].Vendas;
+
+        }
+
+
+    }
+    alert("Vendas feita pelo " + codver + " no mes " + codmes + " : " + venpcod);
+}
+function consultaTotalVendas(Vendas) {
+    var cola = funCola(vendedor);
+    var codver = Number(prompt("Coloque o codigo\n"+cola));
+
+    for (i = 0; i <= Vendas.length; i++) {
+
+        if (codver == Vendas[i].codven) {
+
+            venpcod += Vendas[i].Vendas;
+
+        }
+
+
+    }
+    alert("Vendas feita pelo " + codver + " : " + venpcod);
+
+}
+function calcularMaisVendeu(Vendas) {
+
+    var codmes = Number(prompt("Coloque o mes"));
+
+    var Auxvendas = Vendas[0].Vendas;
+    var auxvendedor;
+
+    for (i = 0; i < Vendas.length; i++) {
+
+        if ((codmes == Vendas[i].mes) && (Auxvendas <= Vendas[i].Vendas)) {
+
+            Auxvendas = Vendas[i].codven;
+
+            auxvendedor = Vendas[i].codven;
+
+        }
+
+
+    }
+    alert("O vendedor que vendeu mais: " + auxvendedor);
+
+}
+function calculaMesMaisVendeu(Vendas) {
+
+    var messes = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+    for (i = 0; i < Vendas.length; i++) {
+
+        messes[Vendas[i].mes - 1] = messes[Vendas[i].mes - 1] + Vendas[i].valor;
+
+    }
+
+    var maior = 0;
+    var mesMaior = 0;
+
+
+    for (i = 0; i < messes.length; i++) {
+
+        maior = messes[i];
+        mesMaior = i + 1;
+
+    }
+}
+function funCola(vendedor){
+
+    var cola = "";
+
+    for (i = 0; i < vendedor.length; i++) {
+
+        cola += vendedor[i].codigo + " - " + vendedor[i].nome + "\n";
+
+    }
+
+    return cola;
+
 }
